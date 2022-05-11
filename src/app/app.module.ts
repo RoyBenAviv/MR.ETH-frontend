0eae803bf1cb6be28fc01c9a15c3dc515f052ee9
Roy Ben Aviv <roybenavivv@gmail.com>
2022-05-11 17:09:58 +0300

    media query changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -75,7 +75,7 @@ export function playerFactory() {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately'
     }),
   ],
   providers: [],
